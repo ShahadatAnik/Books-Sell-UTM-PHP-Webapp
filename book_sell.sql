1179b9ba44806cb13ac6583f3b0778a8f15eb2c5
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-10-01 17:42:20 +0600

    database added

--- book_sell.sql
+++ book_sell.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2022 at 03:56 PM
+-- Generation Time: Oct 01, 2022 at 01:42 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -30,15 +30,21 @@ SET time_zone = "+00:00";
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
-  `password` varchar(500) NOT NULL
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `number` varchar(20) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `approved` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'fahim', 'b94c5dbc799331f0ee036db0c145b5438b9a39dc6984ca5fa1260ca0170df32b');
+INSERT INTO `users` (`id`, `username`, `name`, `email`, `number`, `password`, `approved`) VALUES
+(1, 'fahim', '', '', '', 'b94c5dbc799331f0ee036db0c145b5438b9a39dc6984ca5fa1260ca0170df32b', 1),
+(2, 'hasib', 'Hasib Ar Rafiul Fahim', 'fahad.com66.fk@gmail.com', '01794798101', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', 0),
+(3, 'admin', 'Admin', 'admin@mail.com', '01794798101', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 0);
 
 --
 -- Indexes for dumped tables
@@ -58,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
